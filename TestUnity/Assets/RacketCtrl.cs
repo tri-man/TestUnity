8e4b65c60d70603ff6e2d92f646c61679544ed73
Yoshiaki Hattori <dr_strange_lov@yahoo.co.jp>
2012-07-10 01:10:53 +0900

    そーす修正

--- TestUnity/Assets/RacketCtrl.cs
+++ TestUnity/Assets/RacketCtrl.cs
@@ -6,11 +6,14 @@ public class RacketCtrl : MonoBehaviour {
 	private float Accel = 1000.0f;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+			this.transform.position.x += 1.0f;
+		}else{ Input.GetKeyDown(KeyCode.RightArrow)){
+			}
 	}
 }
